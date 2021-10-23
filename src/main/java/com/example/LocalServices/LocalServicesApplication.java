31e9ecc5efb18b7a1746e0fba00312036e356476
Ian F <ian.fiddeman@gmail.com>
2021-10-23 13:18:52 +0100

    tidy up and fresh repo

---
+++ src/main/java/com/example/LocalServices/LocalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.LocalServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LocalServices/LocalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LocalServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
